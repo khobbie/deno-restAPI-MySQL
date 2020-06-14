8a5149b9b9072f0a6f193f90cff8572bc1ba770e
kwabena <ampahkwabena5@gmail.com>
2020-06-14 17:44:34 +0000

    delete endpoint and controller completed

--- controllers/product.ts
+++ controllers/product.ts
@@ -6,7 +6,7 @@ const getProducts = async (ctx: RouterContext) => {
   if (products.length == 0) {
     ctx.response.status = 404;
     ctx.response.body = {
-      message: 'Available products',
+      message: 'No data found',
       code: '404',
       count: products.length,
       data: products,
@@ -19,6 +19,92 @@ const getProducts = async (ctx: RouterContext) => {
       count: products.length,
       data: products,
     };
+  }
+};
+
+const getSingleProduct = async (ctx: RouterContext) => {
+  const id = ctx.params.id;
+  if (id == undefined) {
+    ctx.response.status = 422;
+    ctx.response.body = {
+      message: 'Invalid or incorrect parameter(s)',
+      code: '422',
+      count: 0,
+      data: null,
+    };
+  } else {
+    const product = await db.query(`SELECT * FROM products WHERE ?? = ?`, [
+      'id',
+      Number(id),
+    ]);
+    if (product.length == 0) {
+      ctx.response.status = 404;
+      ctx.response.body = {
+        message: 'No data found',
+        code: '404',
+        count: product.length,
+        data: product,
+      };
+    } else {
+      ctx.response.status = 200;
+      ctx.response.body = {
+        message: 'Available products',
+        code: '200',
+        count: product.length,
+        data: product,
+      };
+    }
+  }
+};
+
+const deleteSingleProduct = async (ctx: RouterContext) => {
+  const id = ctx.params.id;
+  if (id == undefined) {
+    ctx.response.status = 422;
+    ctx.response.body = {
+      message: 'Invalid or incorrect parameter(s)',
+      code: '422',
+      count: 0,
+      data: null,
+    };
+  } else {
+    const isIdExist = await db.query(`SELECT * FROM products WHERE ?? = ?`, [
+      'id',
+      Number(id),
+    ]);
+    if (isIdExist.length == 0) {
+      ctx.response.status = 404;
+      ctx.response.body = {
+        message: `Product with id: ${id}  does not exits`,
+        code: '404',
+        count: 0,
+        data: null,
+      };
+    } else {
+      const product = await db.query(`DELETE FROM products WHERE ?? = ?`, [
+        'id',
+        Number(id),
+      ]);
+      if (!product.affectedRows) {
+        ctx.response.status = 500;
+        ctx.response.body = {
+          message: 'Failed to execute query',
+          code: '500',
+          count: 0,
+          data: null,
+        };
+      } else {
+        ctx.response.status = 200;
+        ctx.response.body = {
+          message: 'Product deleted',
+          code: '200',
+          count: product.affectedRows,
+          data: {
+            id: id,
+          },
+        };
+      }
+    }
   }
 };
 
@@ -35,15 +121,35 @@ const createProduct = async (ctx: RouterContext) => {
       count: 0,
       data: null,
     };
-    ctx;
   } else {
     let result = await db.execute(
       `INSERT INTO products (description, price) values(?, ?)`,
       [description, Number(price)]
       // ['phone', 20.9]
     );
-    console.log(result.lastInsertId);
+    if (result.lastInsertId) {
+      ctx.response.status = 201;
+      ctx.response.body = {
+        message: 'Created products',
+        code: '201',
+        count: result.affectedRows,
+        data: {
+          id: result.lastInsertId,
+          description: description,
+          price: price,
+          created_at: new Date(),
+        },
+      };
+    } else {
+      ctx.response.status = 500;
+      ctx.response.body = {
+        message: 'Failed to execute query',
+        code: '500',
+        count: 0,
+        data: null,
+      };
+    }
   }
 };
 
-export { getProducts, createProduct };
+export { getProducts, createProduct, getSingleProduct, deleteSingleProduct };
--- server.ts
+++ server.ts
@@ -1,5 +1,10 @@
 import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
-import { getProducts, createProduct } from './controllers/product.ts';
+import {
+  getProducts,
+  createProduct,
+  getSingleProduct,
+  deleteSingleProduct,
+} from './controllers/product.ts';
 
 const app = new Application();
 const router = new Router();
@@ -35,9 +40,9 @@ router
     ctx.response.body = 'Welcome to Deno Rest API with MySQL';
   })
   .get('/products', getProducts)
-  //   .get('/products/:id', getSingleProduct)
-  .post('/products', createProduct);
-//   .put('/products/:id', updateProduct)
-//   .delete('/products/:id', deleteProduct);
+  .get('/products/:id', getSingleProduct)
+  .post('/products', createProduct)
+  //   .put('/products/:id', updateProduct)
+  .delete('/products/:id', deleteSingleProduct);
 await app.listen({ port });
 console.log(`Server is up & running on ${port}`);
